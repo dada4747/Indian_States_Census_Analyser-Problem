14bd2b5c9b7a845856f669d8ca843711e41938e5
dada4747 <adsurerahul96@gmail.com>
2021-06-17 16:28:52 +0530

    ADDED:Test Case to handle exception wrong file path

--- src/test/java/com/bridgelabz/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyserTest.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
@@ -72,7 +73,15 @@ public class CensusAnalyserTest {
         } catch (CensusAnalyserException e) { }
     }
 
-
+    @Test
+    public void givenIndiaStateCode_withWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
 
 
 }
